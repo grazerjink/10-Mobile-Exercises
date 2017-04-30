0e898f014ca4ff7bd1e7500d4b01270fb49c0fbc
grazerjink <grazerjink@gmail.com>
2017-04-30 21:05:26 +0700

    Da hoan thanh (co chinh sua)

--- Homework04/app/src/main/java/com/kj/apps/homework04/MainActivity.java
+++ Homework04/app/src/main/java/com/kj/apps/homework04/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar proMonster1, proMonster2, proGrandma, proClock;
@@ -36,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         run1 = new Runnable() {
             @Override
             public void run() {
-                if (totalGrandma > 0) {
+                if (totalGrandma > 1) {
                     final int rd_time = rd.nextInt(5) + 1;
                     proMonster1.setMax(rd_time);
                     proMonster1.setProgress(rd_time);
@@ -56,12 +59,10 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onFinish() {
-                            totalGrandma--;
+                            updateTotalCookies();
                             total1++;
                             String cookie_eaten = String.format(getString(R.string.total_monster_1).toString(), total1);
                             tvMonster1.setText(cookie_eaten);
-                            String cookie_baked = String.format(getString(R.string.total_grandma).toString(), totalGrandma);
-                            tvGrandma.setText(cookie_baked);
                             proMonster1.setProgress(0);
                             proMonster1.postDelayed(run1, 1000);
                         }
@@ -74,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         run2 = new Runnable() {
             @Override
             public void run() {
-                if (totalGrandma > 0) {
+                if (totalGrandma > 1) {
                     final int rd_time = rd.nextInt(5) + 1;
                     proMonster2.setMax(rd_time);
                     proMonster2.setProgress(rd_time);
@@ -94,12 +95,10 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onFinish() {
-                            totalGrandma--;
+                            updateTotalCookies();
                             total2++;
                             String cookie_eaten = String.format(getString(R.string.total_monster_2).toString(), total2);
                             tvMonster2.setText(cookie_eaten);
-                            String cookie_baked = String.format(getString(R.string.total_grandma).toString(), totalGrandma);
-                            tvGrandma.setText(cookie_baked);
                             proMonster2.setProgress(0);
                             proMonster2.postDelayed(run2, 1000);
                         }
@@ -112,74 +111,7 @@ public class MainActivity extends AppCompatActivity {
         addEvents();
     }
 
-    private void addEvents() {
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                xuLyBatDau();
-            }
-        });
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                xuLyKetThuc();
-            }
-        });
-    }
-
-    private void xuLyKetThuc() {
-        proGrandma.setVisibility(View.INVISIBLE);
-        isStarting = false;
-        if (countDowntimer != null) {
-            countDowntimer.cancel();
-        }
-        if (grandmaIimer != null) {
-            grandmaIimer.cancel();
-        }
-        if (monster1Timer != null) {
-            monster1Timer.cancel();
-        }
-        if (monster2Timer != null) {
-            monster2Timer.cancel();
-        }
-        thietLapBanDau();
-    }
-
-    void thietLapBanDau() {
-        String total_1 = String.format(getString(R.string.total_monster_1).toString(), 0);
-        tvMonster1.setText(total_1);
-        total1 = 0;
-        proMonster1.setMax(100);
-        proMonster1.setProgress(100);
-
-        String total_2 = String.format(getString(R.string.total_monster_2).toString(), 0);
-        tvMonster2.setText(total_2);
-        total2 = 0;
-        proMonster2.setMax(100);
-        proMonster2.setProgress(100);
-
-        String clock_time = String.format(getString(R.string.clock_time).toString(), 120);
-        tvClock.setText(clock_time);
-        proClock.setProgress(120);
-
-        String bakes = String.format(getString(R.string.total_grandma).toString(), 0);
-        tvGrandma.setText(bakes);
-        totalGrandma = 0;
-        proGrandma.setProgress(0);
-    }
-
-    private void xuLyBatDau() {
-        proGrandma.setVisibility(View.VISIBLE);
-        if (isStarting == false) {
-            isStarting = true;
-            thietLapBanDau();
-            startSimulation(120);
-        } else {
-            Toast.makeText(this, "Cuộc thi đang diễn ra....!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    void addControls() {
+    private void addControls() {
         proMonster1 = (ProgressBar) findViewById(R.id.proMonster1);
         proMonster1.setScaleY(5);
         proMonster2 = (ProgressBar) findViewById(R.id.proMonster2);
@@ -203,11 +135,78 @@ public class MainActivity extends AppCompatActivity {
         thietLapBanDau();
     }
 
-    void startSimulation(final int time) {
+    private void addEvents() {
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xuLyBatDau();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xuLyKetThuc();
+            }
+        });
+    }
+
+    private void thietLapBanDau() {
+        String clock_time = String.format(getString(R.string.clock_time).toString(), 120);
+        tvClock.setText(clock_time);
+        proClock.setProgress(120);
+
+        String bakes = String.format(getString(R.string.total_grandma).toString(), 0);
+        tvGrandma.setText(bakes);
+        totalGrandma = 0;
+        proGrandma.setProgress(0);
+
+        String total_1 = String.format(getString(R.string.total_monster_1).toString(), 0);
+        tvMonster1.setText(total_1);
+        total1 = 0;
+        proMonster1.setMax(100);
+        proMonster1.setProgress(100);
+
+        String total_2 = String.format(getString(R.string.total_monster_2).toString(), 0);
+        tvMonster2.setText(total_2);
+        total2 = 0;
+        proMonster2.setMax(100);
+        proMonster2.setProgress(100);
+
+
+    }
+
+    private void xuLyKetThuc() {
+        proGrandma.setVisibility(View.INVISIBLE);
+        isStarting = false;
+        thietLapBanDau();
+        if (countDowntimer != null) {
+            countDowntimer.cancel();
+        }
+        if (grandmaIimer != null) {
+            grandmaIimer.cancel();
+        }
+        if (monster1Timer != null) {
+            monster1Timer.cancel();
+        }
+        if (monster2Timer != null) {
+            monster2Timer.cancel();
+        }
+    }
+
+    private void xuLyBatDau() {
+        proGrandma.setVisibility(View.VISIBLE);
+        if (isStarting == false) {
+            isStarting = true;
+            thietLapBanDau();
+            startSimulation(120);
+        } else {
+            Toast.makeText(this, "Cuộc thi đang diễn ra....!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void startSimulation(final int time) {
         startBakeCookie();
-        run1.run();
-        run2.run();
-        Log.d("TAG", "startSimulation: " + Thread.currentThread().getName());
+        startEating();
         countDowntimer = new CountDownTimer(time * 1000, 992) {
             int i = time;
 
@@ -236,24 +235,34 @@ public class MainActivity extends AppCompatActivity {
         }.start();
     }
 
-    void startBakeCookie() {
+    private void startBakeCookie() {
         grandmaIimer = new CountDownTimer(5000, 996) {
             int i = 0;
 
             @Override
             public void onTick(long millisUntilFinished) {
-                i++;
-                proGrandma.setProgress(i);
             }
 
             @Override
             public void onFinish() {
-                totalGrandma += 10;
-                String cookie_baked = String.format(getString(R.string.total_grandma).toString(), totalGrandma);
-                tvGrandma.setText(cookie_baked);
-                proGrandma.setProgress(0);
+                if (totalGrandma < 50) {
+                    totalGrandma += 10;
+                    String cookie_baked = String.format(getString(R.string.total_grandma).toString(), totalGrandma);
+                    tvGrandma.setText(cookie_baked);
+                }
                 startBakeCookie();
             }
         }.start();
+    }
+
+    private void updateTotalCookies() {
+        totalGrandma--;
+        String cookie_baked = String.format(getString(R.string.total_grandma).toString(), totalGrandma);
+        tvGrandma.setText(cookie_baked);
+    }
+
+    private void startEating() {
+        run1.run();
+        run2.run();
     }
 }
